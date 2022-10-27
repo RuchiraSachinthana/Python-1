fbb74331e6a6b520f19ee743a3dc2b9276ec368f
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2022-10-27 23:22:05 +0530

    added comment

--- 12.py
+++ 12.py
@@ -3,3 +3,5 @@ t = turtle.Turtle()
 t.circle(20) 
 t1=turtle.Turtle() 
 t1.circle(25) 
+
+# comment out the following line to see the effect
--- add two no.py
+++ add two no.py
@@ -5,3 +5,8 @@ num2 = 6.3
 sum = num1 + num2
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+
+// adding two numbers
+
+var num1 = 5.3;
+
